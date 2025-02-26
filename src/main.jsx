049a81c036110cd50b3dbf6bd0251b3b75376739
jbk2 <james@humanbenefits.com>
2025-02-26 14:31:19 +0100

    Further JSX syntax play

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { PlayHeader } from './components/play-elements.jsx'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    {/* <PlayHeader /> */}
     <App />
   </StrictMode>,
 )
